5f2796ee30277ec87792a715c31820314606ad01
Samarth Upadhyay <126957213+samarthupa@users.noreply.github.com>
2024-03-18 21:18:18 +0530

    Update post3.py

--- post3.py
+++ post3.py
@@ -3,11 +3,7 @@ import time
 import streamlit as st
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager, ChromeType
-from selenium.webdriver.chrome.service import ChromiumService
 from bs4 import BeautifulSoup
 
 def search_keywords(keywords, domain):
@@ -16,7 +12,8 @@ def search_keywords(keywords, domain):
     
     # Install and configure Chromium driver
     driver_path = ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
-    service = ChromiumService(executable_path=driver_path)
+    service = webdriver.chrome.service.Service(driver_path)
+
     driver = webdriver.Chrome(service=service, options=options)
     
     results = []
